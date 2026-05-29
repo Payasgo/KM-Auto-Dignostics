6f46f8db0e8c18b134d5695f3e35ab20f1a881b6
payasgo <payasgo55@gmail.com>
2026-05-29 17:59:23 +0530

    code update with animation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-const buttons = document.querySelectorAll(".btn-red,.btn-dark");
-
-buttons.forEach(button => {
-
-    button.addEventListener("mouseenter", () => {
-        button.style.transform = "scale(1.05)";
-        button.style.transition = "0.3s";
-    });
-
-    button.addEventListener("mouseleave", () => {
-        button.style.transform = "scale(1)";
-    });
-
-});
+console.log('Loaded');
